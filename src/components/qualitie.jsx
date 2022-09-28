d08f33cad4aa821a2b7adbd544636ec2e0a37b67
Sergio <sergio-kf@mail.ru>
2022-09-28 21:26:18 +0300

    ESLint

--- src/components/qualitie.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-
-const Qualitie = ({ name, color }) => {
-    return <span className={`badge m-2 bg-${color}`}>{name}</span>
-};
-
-export default Qualitie;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./app/App";
 import "bootstrap/dist/css/bootstrap.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
